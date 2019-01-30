c48f5f86bbe445ab3cc97bfbeeea0cb48e4bd761
week 4 <visualstudio@122-PC33>
2019-01-30 10:37:14 -0500

    week 4

--- C0722784/Program.cs
+++ C0722784/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace C0722784
 {
-    class Program
+    class Village
     {
-        static void Main(string[] args)
-        {
-        }
+        //Node is an ADT
+        //what KIND of DATA do we need in a NODE?
+        public Village nextVillage;
+            public bool IsAstrildeHere = false;
+    
+    
+  
+       
     }
 }
