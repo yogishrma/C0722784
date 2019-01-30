ff63111495a2b43795a5f0e97250d37ddef83554
week 4 <visualstudio@122-PC33>
2019-01-30 11:19:53 -0500

    week 4

--- C0722784/Program.cs
+++ C0722784/Program.cs
@@ -11,8 +11,34 @@ namespace C0722784
         //Node is an ADT
         //what KIND of DATA do we need in a NODE?
         public Village nextVillage;
-            public bool IsAstrildeHere = false;
-    
+        public bool IsAstrildeHere = false;
+
+    }
+    class Countryside
+    {
+        Village Maple = new Village();
+        
+        Village Toronto = new Village();
+        
+        Village Ajax = new Village();
+
+        Village First;
+        Village Last;
+     
+
+            public void Luanch()
+        {
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = Maple;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.nextVillage = Toronto;
+
+        }
     
   
        
