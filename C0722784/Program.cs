f99231702cf6390b56436a9ffdb5f36671c20df6
week 4 <visualstudio@122-PC33>
2019-01-30 11:27:26 -0500

    week 4

--- C0722784/Program.cs
+++ C0722784/Program.cs
@@ -11,6 +11,8 @@ namespace C0722784
         //Node is an ADT
         //what KIND of DATA do we need in a NODE?
         public Village nextVillage;
+        public string VillageName;
+        public string previousVillage;
         public bool IsAstrildeHere = false;
 
     }
